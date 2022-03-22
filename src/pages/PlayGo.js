a033eb97ed4c8c66506e1e657c652121203c73c4
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-03-22 05:24:22 -0500

    Add useEffect hook and methods for calling API with fetch

--- src/pages/PlayGo.js
+++ src/pages/PlayGo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./board.css";
 
@@ -15,31 +15,52 @@ const PlayGo = () => {
 
   const [activePlayer, setActivePlayer] = useState("white");
 
-  const move = (x, y) => {
+  const fetchBoard = async () => {
+    let data;
+    try {
+      const res = await fetch("http://localhost:8080/nodes");
+      data = await res.json();
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+    setBoardNodes(data);
+  };
+  useEffect(() => fetchBoard(), []);
+
+  const postMove = async (move) => {
+    try {
+      await fetch("http://localhost:8080/moves", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(move),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const move = async (x, y) => {
     const isOpenPoint = boardNodes[y][x] === "";
     if (isOpenPoint) {
-      const newNodes = [...boardNodes];
-      newNodes[y][x] = activePlayer;
-      setBoardNodes(newNodes);
+      await postMove({ x: x, y: y, color: activePlayer });
+      fetchBoard();
       setActivePlayer(activePlayer === "white" ? "black" : "white");
     }
   };
 
   return (
-    <div
-      className = "container"
-    >
+    <div className="container">
       <div
         className="board"
         style={{
           gridTemplateRows: `repeat(${boardSize - 1}, 6em) 0`,
           gridTemplateColumns: `repeat(${boardSize - 1}, 6em) 0`,
-
         }}
       >
         {boardNodes.map((col, y) =>
           col.map((node, x) => (
-            <div className="node" key={`Math.random()`}>
+            <div className="node" key={`${Math.random()}`}>
               <div
                 onClick={() => move(x, y)}
                 className={`stone ${node === "" ? "hidden" : null}`}
